7498365e29f239bbcb886d6fffedbff97714aac2
jinsenwu <wujinseng@mafengwo.com>
2019-02-15 12:36:42 +0800

    commit

--- springboot-swagger/src/main/java/cn/springboot/swagger/controller/UserController.java
+++ springboot-swagger/src/main/java/cn/springboot/swagger/controller/UserController.java
@@ -4,16 +4,11 @@ import cn.springboot.swagger.domain.User;
 import cn.springboot.swagger.service.UserService;
 import com.google.common.base.Preconditions;
 import io.swagger.annotations.Api;
-
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
@@ -28,7 +23,7 @@ public class UserController {
     private UserService userService;
 
     @ApiOperation(value = "获取用户", notes = "根据id查询用户信息")
-    @ApiImplicitParam(name = "id", value = "用户id", required = true, dataType = "int")
+    @ApiImplicitParam(name = "id", value = "用户id", required = true, dataType = "string")
     @ResponseBody
     @GetMapping(value = "/queryUser")
     public String queryRuleUser(int id) {
@@ -40,7 +35,14 @@ public class UserController {
             log.error("查询用户失败！", e);
             throw new RuntimeException(e.getMessage());
         }
+    }
 
+    @ApiOperation(value="添加用户信息", notes = "添加用户信息")
+    @ApiImplicitParam(name="user", value="User", required = true, dataType = "User")
+    @PostMapping("/addUser")
+    public Object addUser(@RequestBody User user){
+        log.info("add user:{}", user);
+        return "success";
     }
 
 }
